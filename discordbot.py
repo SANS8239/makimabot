b526c18f50bb20edd0e41688507266b3347fcbf0
SANS8239 <123286625+SANS8239@users.noreply.github.com>
2023-01-22 15:21:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ from distutils.sysconfig import PREFIX
 import discord, asyncio
 from dotenv import load_dotenv
 import os
-from discord.ext import commands
+from discord.ext
 import time
 from youtube_dl import YoutubeDL
 import bs4
